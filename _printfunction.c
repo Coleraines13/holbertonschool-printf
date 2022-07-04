51d831683d9e1c5cf8e725273145717fd9ee54fe
root <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>
2022-07-04 07:56:13 -0700

    printfunction 2

--- _printfunction.c
+++ _printfunction.c
@@ -16,3 +16,44 @@ int _printf(const char *format, ...)
 		{"c", p_char}, {"s", p_str}, {"d", p_dec}, {"i", p_int},
 		{NULL, NULL}
 	};
+
+	va_start(arg, format);
+
+	if (format == NULL || (format[0] == '%' && format [1] == '\0'))
+		return (0);
+
+	i = 0;
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%' && format[i + 1] != '%')
+		{
+			j = 0;
+			flag = 0;
+			while (print[j].p != NULL)
+			{
+				if(format[i + 1] == print[j].print[0])
+				{
+					len = len + print[j].p(arg);
+					flag = 1;
+					i++;
+				}
+				j++;
+			}
+			if (flag == 0)
+			{
+				_putchar(format[i]);
+				len = len + 1;
+			}
+		}
+		else if (format[i] == '%' && format[ i + 1] == '%')
+		{
+			_putchar('%');
+			i++;
+			len = len + 1;
+		}
+		i++;
+	}
+	va_end(arg);
+
+	return (len);
+}
